bab1501299fa78d101a64000334d43a4f2668b3e
Carlosjbermu10 <Carlosjbermu10@gmail.com>
2026-08-11 22:05:34 -0400

    Configuracion de Axios y vercel.json para despliegue

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000/", // <-- Ajusta el puerto y ruta de tu API aquí
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000/",
   headers: {
     "Content-Type": "application/json",
   },
